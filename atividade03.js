c6ef49ae591252b97ff6f8b397a6cfc1e9e026d0
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-07-28 19:21:31 -0300

    Create atividade03.js

---
+++ atividade03.js
@@ -0,0 +1,10 @@
+
+<script>
+    //Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+    var n1=parseInt(prompt("Primeiro numero:  "));
+    var n2=parseInt(prompt("Segundo numero:  "));
+      
+    for(i=n1; i<=n2; i++){
+       document.write(“<b>”+ i + “</b><br>”);
+    }
+</script>
