d6c62588e045f3a97f9a4f2db8d4c3344c08509b
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-07-28 19:05:19 -0300

    Create atividade02.js

---
+++ atividade02.js
@@ -0,0 +1,11 @@
+<script>
+  //Faça um programa que imprima na tela os números de 1 a 30 e do lado apresente a soma dos números existentes.
+  var x=30;
+  var soma=0;
+ 
+  for(i=1 ; i <= x;  i++){
+     document.write(i);
+     soma=soma+i;
+     document.write(" " + soma+"<br>");
+  }   
+</script>
