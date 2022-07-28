6bc23015faf405fad0bd2e8fa3ac46a7cca054bc
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-07-28 18:42:28 -0300

    Create atividade.js

---
+++ atividade.js
@@ -0,0 +1,9 @@
+<script>
+   //Faça um programa que imprima na tela os números de 1 a 20.
+   var y = 1;
+
+   while (y <= 20) {
+      document.write("<b>"+y+ "</b><br>");
+      y++; 
+   }
+</script>
