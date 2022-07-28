d1b919713c74a3313180ff72bc86db51879172c6
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-07-28 20:08:07 -0300

    Create atividade04.js

---
+++ atividade04.js
@@ -0,0 +1,8 @@
+<script>
+   //Faça um programa que imprima na tela apenas os números ímpares entre 1 e 100.
+   var x=100;
+   for(y=1; y<=x; y++){
+     if((y%2)==1)
+       document.write(y+"<br>");      
+   }
+</script>
